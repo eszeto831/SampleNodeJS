4112bd7c6d10c481d9f1bfe4660c9d58b919063e
Edmond Szeto <eszeto831@gmail.com>
2021-09-23 03:15:15 -1000

    try again

--- server/index.js
+++ server/index.js
@@ -1,11 +1,12 @@
 // server/index.js
 
-const express = require("express"),
+const express = require('express'),
   app = express(),
   mysql = require('mysql'), // import mysql module
   cors = require('cors'),
   bodyParser = require('body-parser');
 
+// setup database
 db = mysql.createConnection({
 	  host: "107.180.41.89",
 	  user: "seatoadDBTester",
@@ -13,21 +14,17 @@ db = mysql.createConnection({
 	  database: "TestBackendUser"
 	});
 
-const PORT = process.env.PORT || 3001;
+// make server object that contain port property and the value for our server.
+var server = {
+  port: 4040
+};
 
 // routers
 const usersRouter = require('./routes/users');
 // use the modules
 app.use(cors())
 app.use(bodyParser.json());
-
-app.get("/api", (req, res) => {
-  res.json({ message: "Hello from server!" });
-});
-
 // use router
-app.use('api/users', usersRouter);
+app.use('/users', usersRouter);
 
-app.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
-});
+app.listen( server.port , () => console.log(`Server started, listening on port: ${server.port}`));
--- server/routes/users.js
+++ server/routes/users.js
@@ -14,4 +14,20 @@ router.get('/list', function(req, res) {
   })
 });
 
+// create new user
+router.post('/new', function(req, res) {
+  let sql = `INSERT INTO users(name, gender) VALUES (?)`;
+  let values = [
+    req.body.name,
+    req.body.gender
+  ];
+  db.query(sql, [values], function(err, data, fields) {
+    if (err) throw err;
+    res.json({
+      status: 200,
+      message: "New user added successfully"
+    })
+  })
+});
+
 module.exports = router;
